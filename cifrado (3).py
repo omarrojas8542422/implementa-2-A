cce32ee1263d6a5051c4817c89dacd52eb5db316
Francisco220609 <josefrancisco13169@gmail.com>
2025-03-19 12:17:26 -0600

    trabaje

--- cifrado (3).py
+++ cifrado (3).py
@@ -3,12 +3,12 @@ if texto== texto.upper():
     abc="ABCDEFGHIJKLMNOPQRSTVXYZ"
 else:
     abc="abcdefghijklmnopqrstuvwxyz"
-k=int(input("Valor de desplazamiento "))
+k=int(input("40"))
 cifrad=""
 for c in texto:
     if c in abc:
         cifrad+= abc[(abc.index(c)+k)%(len(abc))]
     else:
         cifrad+=c
-print("Texto cifrado: ", cifrad)
+print("hola mundo", cifrad)
                     
